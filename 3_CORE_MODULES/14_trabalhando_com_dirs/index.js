6e92239f5bed9b9c5422087433486dfb15e268f5
fabricio <fabriciosantosc11@gmail.com>
2025-11-19 14:25:42 -0300

    Update index.js

--- 3_CORE_MODULES/14_trabalhando_com_dirs/index.js
+++ 3_CORE_MODULES/14_trabalhando_com_dirs/index.js
@@ -1,2 +1,8 @@
 const fs = require('fs')
 
+if(!fs.existsSync('./minhapasta')){
+    console.log("nao existe essa pasta!")
+    fs.mkdirSync('minhapasta')
+} else if (fs.existsSync('./minhapasta')){
+    console.log("Agora existe essa pasta!")
+}
