80d023dce2129b8cfaa2c452cde00d48362de326
fabricio <fabriciosantosc11@gmail.com>
2025-12-15 20:26:47 -0300

    tela de cadastro

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -14,6 +14,7 @@ const User = require('./models/User')
 
 // import routes
 const toughtsRoutes = require('./routes/toughtsRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 // import controller
 const ToughtController = require('./controllers/ToughtController')
@@ -68,6 +69,7 @@ app.use((req,res,next) => {
 
 //rotas
 app.use('toughts', toughtsRoutes)
+app.use('/', authRoutes)
 
 app.get('/', ToughtController.showToughts)
 
