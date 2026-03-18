878ab5d79882c5cbafb6ae0f7cccb8f934740340
fabricio <fabriciosantosc11@gmail.com>
2026-03-18 11:17:04 -0300

    finalizando atualizacao

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -164,14 +164,31 @@ module.exports = class UserController {
             res.status(422).json({message: 'O telefone é obrigatorio!'})
             return
         }
+        user.phone = phone
 
-        if(!password) {
-            res.status(422).json({message: 'A senha é obrigatoria!'})
+        if(password != confirmpassword) {
+            res.status(422).json({message: 'As senhas nao conferem!'})
+            return
+        }else if(password === confirmpassword && password != null) {
+            // creating password
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+
+        try {
+            // returns user updated data
+            await User.findOneAndUpdate(
+                {_id: user._id},
+                {$set: user},
+                {new: true}
+            )
+            res.status(200).json({message: 'Usuario atualizado com sucesso!'})
+        } catch (err) {
+            res.status(500).json({message: err})
             return
         }
-        if(!confirmpassword) {
-            res.status(422).json({message: 'A confirmação de senha é obrigatoria!'})
-            return
-        }
+     
     }
 }
