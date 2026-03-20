e51d0b8b682b92d378051d2b8282d8285317c64f
fabricio <fabriciosantosc11@gmail.com>
2026-03-20 15:53:35 -0300

    estruturando navbar e footer

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* components */ 
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+/* pages */ 
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
@@ -6,11 +12,13 @@ import Register from "./components/pages/Auth/Register";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+       <Footer/>
     </BrowserRouter>
   );
 }
