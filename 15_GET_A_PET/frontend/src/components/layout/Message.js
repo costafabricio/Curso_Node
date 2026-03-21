4ce125f58751785e795c17b362d4ac7a4e97ed4c
fabricio <fabriciosantosc11@gmail.com>
2026-03-21 18:48:08 -0300

    finalizando componente de flash message

--- 15_GET_A_PET/frontend/src/components/layout/Message.js
+++ 15_GET_A_PET/frontend/src/components/layout/Message.js
@@ -1,12 +1,29 @@
-import { useDebugValue, useState } from "react";
+import {useState, useEffect } from "react";
+import bus from "../../utils/bus";
 
 import styles from './Message.module.css'
 
 function Message() {
+    const [visibility, setvisibility] = useState(false)
+    const [message, setMessage] = useState('')
     const [type, setType] = useState("")
 
+    useEffect(() => {
+        bus.addListener('flash', ({message, type}) => {
+            setvisibility(true)
+            setMessage(message)
+            setType(type)
+
+          setTimeout(() => {
+            setvisibility(false)
+          }, 3000)
+        }) 
+    }, [])
+
     return(
-        <div className={`${styles.message} ${styles[type]}`}>Minha messagem</div>
+        visibility && (
+           <div className={`${styles.message} ${styles[type]}`}>{message}</div>
+        )
     )
 }
 
--- 15_GET_A_PET/frontend/src/hooks/useAuth.js
+++ 15_GET_A_PET/frontend/src/hooks/useAuth.js
@@ -2,9 +2,15 @@ import api from '../utils/api'
 
 import { useState, useEffect } from 'react'
 import {useHistory} from 'react-router-dom'
+import useFlashMessage from './useFlashMessage'
 
 export default function useAuth() {
+    const {setFlashMessage} = useFlashMessage()
+
     async function register(user) {
+        let msgText = 'cadastro realizaddo com sucesso!'
+        let msgType = 'success'       
+
         try {
           const data = await api.post('/users/register', user)
             .then((response) => {
@@ -12,8 +18,11 @@ export default function useAuth() {
             })
             console.log(data)
         } catch (error) {
-            console.log(error)
+            msgText = error.response.data.message
+            msgType = 'error'    
         }
+
+        setFlashMessage(msgText, msgType)
     }
 
     return {register}
