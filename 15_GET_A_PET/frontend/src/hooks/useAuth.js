b6e4a551385ad764619b24319b539fbbde1e656b
fabricio <fabriciosantosc11@gmail.com>
2026-03-21 19:14:36 -0300

    autenticando usuario cadastrado

--- 15_GET_A_PET/frontend/src/hooks/useAuth.js
+++ 15_GET_A_PET/frontend/src/hooks/useAuth.js
@@ -1,22 +1,32 @@
 import api from '../utils/api'
 
 import { useState, useEffect } from 'react'
-import {useHistory} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import useFlashMessage from './useFlashMessage'
 
 export default function useAuth() {
+    const [authenticated, setAuthenticated] = useState(false)
     const {setFlashMessage} = useFlashMessage()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if(token) {
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    },[])
 
     async function register(user) {
         let msgText = 'cadastro realizaddo com sucesso!'
         let msgType = 'success'       
 
         try {
-          const data = await api.post('/users/register', user)
-            .then((response) => {
-                return response.data
-            })
-            console.log(data)
+           const response = await api.post('/users/register', user)
+           const data = response.data
+
+             await authUser(data)
         } catch (error) {
             msgText = error.response.data.message
             msgType = 'error'    
@@ -25,5 +35,13 @@ export default function useAuth() {
         setFlashMessage(msgText, msgType)
     }
 
-    return {register}
+    async function authUser(data) {
+        setAuthenticated(true)
+
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+        navigate('/')
+    }
+
+    return { authenticated, register }
 }
