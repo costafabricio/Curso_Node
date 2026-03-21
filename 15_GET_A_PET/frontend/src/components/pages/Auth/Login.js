aef644ae14b397bd35d9375ab77be5f28fc5a576
fabricio <fabriciosantosc11@gmail.com>
2026-03-21 20:38:36 -0300

    criando formulario de login

--- 15_GET_A_PET/frontend/src/components/pages/Auth/Login.js
+++ 15_GET_A_PET/frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,40 @@
+import { useState, useContext } from "react"
+
+import { Link } from "react-router-dom"
+import Input from '../../form/Input'
+
+import styles from '../../form/Form.module.css'
+
+/* Context */
+import { Context } from "../../../context/UserContext"
+
 function Login() {
+
+    function handleChange() {}
+
     return(
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                   text="Email"
+                   type="email"
+                   name="email"
+                   placeholder="Digite o seu email"
+                   handleOnChange={handleChange}
+                 />
+                <Input
+                   text="Senah"
+                   type="password"
+                   name="password"
+                   placeholder="Digite a sua senha"
+                   handleOnChange={handleChange}
+                 />
+                 <input type="submit" value="Entrar"/>
+            </form>
+            <p>
+                Não tem conta? <Link to="/register">Clique aqui.</Link>
+            </p>
         </section>
     )
 }
