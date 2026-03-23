905002f54bf4cf11136430c6c45cf6ffb583800c
fabricio <fabriciosantosc11@gmail.com>
2026-03-23 17:24:14 -0300

    atualizacao do usuario

--- 15_GET_A_PET/frontend/src/components/pages/User/Profile.js
+++ 15_GET_A_PET/frontend/src/components/pages/User/Profile.js
@@ -1,14 +1,18 @@
+import { useEffect, useState } from 'react'
 import api from '../../../utils/api'
 
 import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
 
 import Input from '../../form/Input'
-import { useEffect, useState } from 'react'
+
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 
 function Profile() {
     const [user, setUser] = useState({})
     const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
 
     useEffect(() => {
         api.get('/users/checkuser', {
@@ -22,10 +26,37 @@ function Profile() {
     },[token])
 
     function onFileChange(e) {
-
+        setUser({...user, [e.target.name]: e.target.files[0]})
     }
-    function handleChange(e) {
 
+    function handleChange(e) {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        await Object.keys(user).forEach((key) =>
+            formData.append(key, user[key]) 
+        )
+
+        const data = await api.patch(`/users/edit/${user._id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((response) => {
+            return response.data
+        }).catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
     }
 
     return(
@@ -34,7 +65,7 @@ function Profile() {
                <h1>Perfil</h1>
                <p>preview imagem</p>
             </div>
-            <form className={formStyles.form_container}>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
                   text="Imagem"
                   type="file"
