66957e584fc64c7a07ef3e458fbae347a483759d
fabricio <fabriciosantosc11@gmail.com>
2026-03-25 20:42:42 -0300

    criando pagina de edicao de pet

---
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/EditPet.js
@@ -0,0 +1,21 @@
+import api from "../../../utils/api"
+
+import styles from './AddPet.module.css'
+
+import PetForm from '../../form/PetForm'
+
+/* hooks */
+import useFlashMessage from "../../../hooks/useFlashMessage"
+
+function EditPet() {
+    return(
+        <section>
+           <div className={styles.addpet_header}>
+              <h1>Editando o Pet: pet.name</h1>
+              <p>Depois da edição os dados serão atualizados no sistema</p>
+           </div>
+        </section>
+    )
+}
+
+export default EditPet
