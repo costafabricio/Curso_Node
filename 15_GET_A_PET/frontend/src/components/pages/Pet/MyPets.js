dafa1d0e0601013ec953e8dec74cf03c11ceb6b5
fabricio <fabriciosantosc11@gmail.com>
2026-03-25 11:33:54 -0300

    exibindo os pets na dashboard

--- 15_GET_A_PET/frontend/src/components/pages/Pet/MyPets.js
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/MyPets.js
@@ -2,6 +2,8 @@ import api from '../../../utils/api'
 
 import { useEffect, useState } from "react"
 
+import styles from './Dashboard.module.css'
+
 import {Link} from 'react-router-dom'
 
 import RoundedImage from '../../layout/RoundedImage'
@@ -31,9 +33,31 @@ function MyPets() {
                 <Link to='/pet/add'>Cadastrar Pet </Link>
             </div>
             <div>
-                {pets.length > 0 && (
-                    <p>Meus pets cadastrados</p>
-                )}
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                     <div key={pet._id}>
+                      <RoundedImage
+                        src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`} 
+                        alt={pet.name}
+                        width="75px"
+                          />
+                        <span className='bold'>{pet.name}</span>
+                        <div className={styles.action}>
+                            {pet.available ? (
+                                <>
+                                  {pet.adopter && (
+                                    <button>Concluir Adoção</button>
+                                  )}
+                                  <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                                  <button>Excluir</button>  
+                                </>
+                            ): (
+                                <p>Pet ja adotado</p>
+                            )}
+                        </div>
+                     </div>
+                    ))
+                }
                 {pets.length === 0 && <p>Não há pets cadastrados</p>}
             </div>
         </section>
