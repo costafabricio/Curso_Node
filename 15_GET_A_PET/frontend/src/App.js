1c4512e1411e0a37e0528ea01ea3df331d7cb383
fabricio <fabriciosantosc11@gmail.com>
2026-03-27 19:34:50 -0300

    pagina de detalhes do pet

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -14,6 +14,7 @@ import Profile from "./components/pages/User/Profile";
 import MyPets from "./components/pages/Pet/MyPets";
 import AddPet from "./components/pages/Pet/AddPet";
 import EditPet from "./components/pages/Pet/EditPet";
+import PetDetails from "./components/pages/Pet/PetDetails";
 
 /* context */
 import { UserProvider } from "./context/UserContext"; 
@@ -33,6 +34,7 @@ function App() {
                   <Route path="/user/profile" element={<Profile />} />
                   <Route path="/pet/mypets" element={<MyPets />} />
                   <Route path="/pet/add" element={<AddPet />} />
+                  <Route path="/pet/:id" element={<PetDetails />} />
                   <Route path="/pet/edit/:id" element={<EditPet />} />
                 </Routes>
              </Container>
---
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/PetDetails.js
@@ -0,0 +1,17 @@
+import api from '../../../utils/api'
+
+import { useState, useEffect } from 'react'
+import {useParams, Link} from 'react-router-dom'
+
+import styles from './PetDetails.module.css'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+function PetDetails() {
+    return(
+        <h1>detalhes</h1>
+    )
+}
+
+export default PetDetails
