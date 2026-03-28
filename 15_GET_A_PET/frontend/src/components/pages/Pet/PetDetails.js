41e57647a8d99d751a2f041f9c084c3e831e88d0
fabricio <fabriciosantosc11@gmail.com>
2026-03-28 19:14:50 -0300

    funcao de agendar visita

--- 15_GET_A_PET/frontend/src/components/pages/Pet/PetDetails.js
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/PetDetails.js
@@ -11,7 +11,7 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
 function PetDetails() {
     const [pet, setPet] = useState({})
     const {id} = useParams()
-    const setFlashMessage = useFlashMessage()
+    const {setFlashMessage} = useFlashMessage()
     const [token] = useState(localStorage.getItem('token') || '')
 
     useEffect(() => {
@@ -19,6 +19,21 @@ function PetDetails() {
             setPet(response.data.pet)
         })
     },[id])
+
+    async function schedule() {
+        let msgType = 'success'
+
+        const data = await api.patch(`pets/schedule/${pet._id}`, {
+            Authorization: `Bearer ${JSON.parse(token)}`
+        }).then((response) => {
+            return response.data
+        }).catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
 
     return(
         <>
@@ -44,7 +59,7 @@ function PetDetails() {
                     <span className='bold'>Idade:</span> {pet.age} anos    
                 </p>
                 {token ? (
-                    <button>Solicitar uma visita</button>
+                    <button onClick={schedule}>Solicitar uma visita</button>
                 ) : (
                     <p>Voce precisa <Link to='/register'>criar uma conta</Link> para solicitar a visita</p>
                 )}
